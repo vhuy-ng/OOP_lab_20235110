ec5d3f54dcd892efba7b3406b0b74a432aedd2f2
Huy <148065433+vhuy-ng@users.noreply.github.com>
2024-09-25 23:06:49 +0700

    rename

--- lab01/Exercise2.java
+++ lab01/Exercise2.java
@@ -1,10 +1,10 @@
 /* bai 2.2.5 */
 
-package Lab01;
+package lab01;
 
 import java.util.Scanner;
 
-public class Calc2Double{
+public class Exercise2{
     public static void main(String [] args){
         double a, b;
         Scanner sc = new Scanner(System.in);
@@ -12,9 +12,13 @@ public class Calc2Double{
         a = sc.nextDouble();
         System.out.print("num2: ");
         b = sc.nextDouble();
+
         System.out.println("sum: "+ (a+b));
+
         System.out.println("difference: "+ (a-b));
+
         System.out.println("product: "+ (a*b));
+        
         if(b == 0) System.out.println("divisor error");
         else System.out.print("quotient: "+ (a/b));
     }
