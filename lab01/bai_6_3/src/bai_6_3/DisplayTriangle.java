bfb4fab504d031460a7dad558639b465e15531d6
Huy <148065433+vhuy-ng@users.noreply.github.com>
2024-11-12 00:04:39 +0700

    add ex 6.1+6.2 & ex 6.3

---
+++ lab01/bai_6_3/src/bai_6_3/DisplayTriangle.java
@@ -0,0 +1,26 @@
+package bai_6_3;
+
+import java.util.Scanner;
+
+public class DisplayTriangle {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		
+		for(int i = 1;i<=n;i++)
+		{
+			for(int j = n-i; j>0; j--)
+			{
+				System.out.print(" ");
+				
+			}
+			for(int j = 1; j<= 2*i-1; j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+		
+	}
+}
